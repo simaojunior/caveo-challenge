8de1239ed5d5870428258cf0eee4cec395e6aae4
Simão Júnior <simao.msjr@gmail.com>
2025-08-18 04:24:50 -0300

    feat(app): integrate setupRoutes function into createApp

--- server/src/main/config/app.ts
+++ server/src/main/config/app.ts
@@ -1,9 +1,11 @@
 import Koa from 'koa';
 import { setupMiddlewares } from './middlewares';
+import { setupRoutes } from './routes';
 
 export const createApp = (): Koa => {
   const app = new Koa();
   setupMiddlewares(app);
+  setupRoutes(app);
 
   return app;
 };
