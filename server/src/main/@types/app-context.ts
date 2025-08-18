b245f7bce3ee4c1a14a5eea75456ffb18d4ba749
Simão Júnior <simao.msjr@gmail.com>
2025-08-18 01:25:57 -0300

    feat(context): add application state and request context types

---
+++ server/src/main/@types/app-context.ts
@@ -0,0 +1,26 @@
+import type { Request, ParameterizedContext } from 'koa';
+
+/**
+ * Declaration for application state object.
+ * Use this as context type in controllers.
+ */
+export type AppState = {
+  user?: {
+    id: string;
+    jwt?: string;
+    roles?: ('admin' | 'user')[];
+  };
+};
+
+export interface IAppRequest<RequestBodyT = any> extends Request {
+  body?: RequestBodyT;
+  files?: Record<string, any>;
+}
+
+export interface IAppDefaultContext<RequestBodyT>
+  extends ParameterizedContext<AppState> {
+  request: IAppRequest<RequestBodyT>;
+}
+
+export type IAppContext<RequestBodyT = unknown> =
+  IAppDefaultContext<RequestBodyT>;
