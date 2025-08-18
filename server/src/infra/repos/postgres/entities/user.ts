d126551ded28caf7e3834ce6e21d5972b5123d48
Simão Júnior <simao.msjr@gmail.com>
2025-08-18 15:48:29 -0300

    feat(user): create typeorm user entity

---
+++ server/src/infra/repos/postgres/entities/user.ts
@@ -0,0 +1,45 @@
+import { UserRole } from '@/domain/entities/user';
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Index,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn('uuid')
+  id!: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  name!: string;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
+  @Index('idx_user_email')
+  email!: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.USER,
+  })
+  role!: UserRole;
+
+  @Column({ name: 'is_onboarded', type: 'boolean', default: false })
+  isOnboarded!: boolean;
+
+  @Column({ name: 'external_id', type: 'varchar', nullable: true })
+  externalId?: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt!: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at', nullable: true })
+  deletedAt?: Date | null;
+}
