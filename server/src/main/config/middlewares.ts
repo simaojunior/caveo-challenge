3c19a2389955ebf811318ce283865bff2bf53a4e
Simão Júnior <simao.msjr@gmail.com>
2025-08-18 04:57:07 -0300

    feat(logging): integrate makeRequestLoggingMiddleware into middleware setup

--- server/src/main/config/middlewares.ts
+++ server/src/main/config/middlewares.ts
@@ -1,8 +1,11 @@
 import type Koa from 'koa';
 import { koaBody } from 'koa-body';
 import { ErrorMiddleware } from '../middlewares/error-handler';
+import { makeRequestLoggingMiddleware } from '../factories/application/middlewares/request-logging';
 
 export const setupMiddlewares = (app: Koa): void => {
+  app.use(makeRequestLoggingMiddleware());
+
   app.use(koaBody({
     json: true,
     text: false,
