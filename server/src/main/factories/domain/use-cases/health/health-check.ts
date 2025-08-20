b097579037652a6b827581e5ec3b9bb062b3e2cd
Simão Júnior <simao.msjr@gmail.com>
2025-08-19 23:30:30 -0300

    feat(health): add db status

--- server/src/main/factories/domain/use-cases/health/health-check.ts
+++ server/src/main/factories/domain/use-cases/health/health-check.ts
@@ -1,5 +1,6 @@
 import { setupHealthCheck, type HealthCheckUseCase } from '@/domain/use-cases';
+import { makePgConnection } from '@/main/factories/infra/repos/helpers/connection';
 
 export const makeHealthCheck = (): HealthCheckUseCase => {
-  return setupHealthCheck();
+  return setupHealthCheck(makePgConnection());
 };
