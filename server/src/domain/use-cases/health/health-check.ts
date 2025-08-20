e4964663aad0dfcc5b4d618753b5fd64ac7dc2a3
Simão Júnior <simao.msjr@gmail.com>
2025-08-19 23:30:30 -0300

    feat(health): add db status

--- server/src/domain/use-cases/health/health-check.ts
+++ server/src/domain/use-cases/health/health-check.ts
@@ -1,29 +1,42 @@
+import type { PgConnection } from '@/infra/repos/postgres/helpers';
+
 export enum HealthStatus {
-	UP = 'UP',
-	DOWN = 'DOWN',
+  UP = 'UP',
+  DOWN = 'DOWN',
 }
 
 type Output = {
-	status: HealthStatus;
-	timestamp: string;
-	uptime: number;
-	database?: {
-		status: 'UP' | 'DOWN';
-	};
+  status: HealthStatus;
+  timestamp: string;
+  uptime: number;
+  database?: {
+    status: 'UP' | 'DOWN';
+  };
 };
 
 export type HealthCheckUseCase = () => Promise<Output>;
 
-type Setup = () => HealthCheckUseCase;
+type Setup = (connection: PgConnection) => HealthCheckUseCase;
 
-export const setupHealthCheck: Setup = () => {
-
+export const setupHealthCheck: Setup = (connection) => {
   return async (): Promise<Output> => {
     const baseHealth: Output = {
       status: HealthStatus.UP,
       timestamp: new Date().toISOString(),
       uptime: Math.floor(process.uptime()),
     };
+
+    if (connection) {
+      try {
+        const dataSource = connection.getInstance();
+        await dataSource.query('SELECT 1');
+
+        baseHealth.database = { status: 'UP' };
+      } catch {
+        baseHealth.database = { status: 'DOWN' };
+        baseHealth.status = HealthStatus.DOWN;
+      }
+    }
 
     return baseHealth;
   };
