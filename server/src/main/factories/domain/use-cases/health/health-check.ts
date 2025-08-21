0824d5554db070821c9849fe15c2c9fd444aca17
Simão Júnior <simao.msjr@gmail.com>
2025-08-21 20:39:57 -0300

    refactor(health): correct import path for makePgConnection

--- server/src/main/factories/domain/use-cases/health/health-check.ts
+++ server/src/main/factories/domain/use-cases/health/health-check.ts
@@ -1,5 +1,5 @@
 import { setupHealthCheck, type HealthCheckUseCase } from '@/domain/use-cases';
-import { makePgConnection } from '@/main/factories/infra/repos/helpers/connection';
+import { makePgConnection } from '@/main/factories/infra/repos/postgres/helpers/connection';
 
 export const makeHealthCheck = (): HealthCheckUseCase => {
   return setupHealthCheck(makePgConnection());
