dac1da0415403c27fc96a29ef43daa98edac8e53
Simão Júnior <simao.msjr@gmail.com>
2025-08-21 20:41:09 -0300

    feat(routes): add user routes for authentication

--- server/src/main/config/routes.ts
+++ server/src/main/config/routes.ts
@@ -1,12 +1,13 @@
 import type Koa from 'koa';
 import Router from '@koa/router';
 
-import { setupHealthRoutes } from '../routes';
+import { setupHealthRoutes, setupUserRoutes } from '../routes';
 
 export const setupRoutes = (app: Koa): void => {
   const router = new Router();
 
   setupHealthRoutes(router);
+  setupUserRoutes(router);
   app.use(router.routes());
   app.use(router.allowedMethods());
 };
