4e54bf3242f82cd4e2a60282c438eb199ac87d3c
Simão Júnior <simao.msjr@gmail.com>
2025-08-22 15:55:07 -0300

    refactor(controller): update import and return type for SigninOrRegisterController

--- server/src/main/factories/application/controllers/auth/signin-or-register.ts
+++ server/src/main/factories/application/controllers/auth/signin-or-register.ts
@@ -1,6 +1,7 @@
-import { SigninOrRegister } from '@/application/controllers';
+import { SigninOrRegisterController } from '@/application/controllers';
 import { makeSigninOrRegisterUseCase } from '@/main/factories/domain/use-cases';
 
-export const makeSigninOrRegisterController = (): SigninOrRegister => {
-  return new SigninOrRegister(makeSigninOrRegisterUseCase());
-};
+export const makeSigninOrRegisterController =
+  (): SigninOrRegisterController => {
+    return new SigninOrRegisterController(makeSigninOrRegisterUseCase());
+  };
