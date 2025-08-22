8619ab368242ab9b3896156e15769225f80d22ab
Simão Júnior <simao.msjr@gmail.com>
2025-08-22 01:29:20 -0300

    fix(contracts): remove duplicate export of controller and clean up index

--- server/src/application/contracts/index.ts
+++ server/src/application/contracts/index.ts
@@ -1,6 +1,5 @@
 export type * from './http';
+export * from './controller';
 export type * from './middleware';
 export type * from './error-handler';
 export type * from './request-logger';
-export * from './controller';
-export type * from './routes';
