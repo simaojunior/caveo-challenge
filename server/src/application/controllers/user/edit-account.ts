f91d74ae8b7041168ad5375ff474770e87d7681d
Simão Júnior <simao.msjr@gmail.com>
2025-08-22 15:33:56 -0300

    feat(user): implement EditAccount controller for user account editing

---
+++ server/src/application/controllers/user/edit-account.ts
@@ -0,0 +1,46 @@
+import z from 'zod';
+import { UserRole } from '@/domain/entities/user';
+import { httpResponse } from '@/application/helpers/http';
+import {
+  Controller,
+  type HttpRequest,
+  type HttpResponse,
+} from '@/application/contracts';
+import type { EditAccountUseCase } from '@/domain/use-cases';
+
+const inputSchema = z.object({
+  userId: z.uuid(),
+  name: z.string().min(1, 'Name must not be empty').optional(),
+  role: z.enum(UserRole).optional(),
+}).refine(
+  (data) => data.name !== undefined || data.role !== undefined,
+  {
+    message: 'At least one field (name or role) must be provided',
+    path: ['general'],
+  },
+);
+
+export class EditAccount extends Controller {
+  constructor(private readonly editAccountUseCase: EditAccountUseCase) {
+    super();
+  }
+
+  async handle(request: HttpRequest): Promise<HttpResponse> {
+    const  { name, role, userId } = inputSchema.parse(request.body);
+
+    const { id, email, isOnboarded } = await this.editAccountUseCase({
+      userId,
+      currentUserId: request.user?.id,
+      currentUserRoles: request.user?.roles,
+      name,
+      role,
+    });
+
+    return httpResponse.ok({
+      id,
+      name,
+      email,
+      isOnboarded,
+    });
+  }
+}
