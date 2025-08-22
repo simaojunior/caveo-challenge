e48c0b6245966e5fb06c0a2544f7d92293abcd79
Simão Júnior <simao.msjr@gmail.com>
2025-08-22 15:37:47 -0300

    refactor(auth): remove unused 'me' route and related middlewares

--- server/src/main/routes/auth.ts
+++ server/src/main/routes/auth.ts
@@ -1,9 +1,7 @@
 import type Router from '@koa/router';
 import { adaptKoaRoute as adapt } from '@/main/adapters/koa-router';
-import { auth, authorize } from '@/main/middlewares';
-import { makeSigninOrRegisterController, makeMeController } from '../factories/application/controllers';
+import { makeSigninOrRegisterController } from '../factories/application/controllers';
 
 export const setupAuthRoutes = (router: Router): void => {
   router.post('/auth', adapt(makeSigninOrRegisterController()));
-  router.get('/me', auth, authorize, adapt(makeMeController()));
 };
