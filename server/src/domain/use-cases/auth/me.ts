d210717bc8786f40bbefe3280bd49af607951bbd
Simão Júnior <simao.msjr@gmail.com>
2025-08-22 01:28:14 -0300

    feat(use-cases): implement MeUseCase for user retrieval

---
+++ server/src/domain/use-cases/auth/me.ts
@@ -0,0 +1,38 @@
+import type { UserRole } from '@/domain/entities/user';
+import { ResourceNotFound } from '@/application/errors';
+import type { IFindUser } from '@/domain/contracts/repos/user';
+
+type Input = {
+  id: string;
+  roles: UserRole[];
+}
+
+type Output = {
+  id: string
+  name?: string
+  email: string
+  role: UserRole
+  isOnboarded: boolean
+}
+
+export type MeUseCase = (input: Input) => Promise<Output>;
+
+type Setup = (userRepo: IFindUser) => MeUseCase;
+
+export const setupMe: Setup = (userRepo) => {
+  return async ({ id }) => {
+    const user = await userRepo.findUser({ id });
+
+    if (!user) {
+      throw new ResourceNotFound(`User with ID ${id} not found`);
+    }
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      isOnboarded: user.isOnboarded,
+    };
+  };
+};
