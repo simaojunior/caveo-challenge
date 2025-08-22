431477c13f4d3829e21c2acd1ad6c3dc295c6683
Simão Júnior <simao.msjr@gmail.com>
2025-08-22 01:33:27 -0300

    fix(tests): update test inclusion and exclusion patterns in vitest config

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    include: ['**/*.{test,spec,e2e.test}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: 'verbose',
     coverage: {
       enabled: true,
@@ -11,7 +12,7 @@ export default defineConfig({
       reporter: ['text', 'text-summary', 'html'],
       all: true,
       include: ['src/**/*.ts'],
-      exclude: ['**/*.test.ts', 'src/tests/**'],
+      exclude: ['**/*.test.ts', '**/*.e2e.test.ts', 'src/tests/**'],
     },
   },
 });
