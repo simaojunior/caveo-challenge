06e67092826e0cff6f3dd7b5a660afc82b994dbc
Simão Júnior <simao.msjr@gmail.com>
2025-08-23 16:52:42 -0300

    feat(user): add route for fetching users with admin authorization

--- server/src/main/routes/user.ts
+++ server/src/main/routes/user.ts
@@ -1,10 +1,11 @@
 import type Router from '@koa/router';
 
-import { auth, authorize } from '@/main/middlewares';
+import { auth, authorize, authorizeAdmin } from '@/main/middlewares';
 import { adaptKoaRoute as adapt } from '@/main/adapters/koa-router';
-import { makeEditAccountController, makeGetMeController } from '../factories/application/controllers';
+import { makeEditAccountController, makeGetMeController, makeSearchUsersController } from '../factories/application/controllers';
 
 export const setupUserRoutes = (router: Router): void => {
   router.get('/me', auth, authorize, adapt(makeGetMeController()));
   router.put('/edit-account', auth, authorize, adapt(makeEditAccountController()));
+  router.get('/users', auth, authorizeAdmin, adapt(makeSearchUsersController()));
 };
